66480c221f5ab30ebb3d83e2947d0bf1f5be58c0
平安喜乐 <2014567296@qq.com>
2024-05-06 23:34:30 +0800

    feat: 首页扑克牌样式调整

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,15 +1,11 @@
 <script setup>
-
+import ArticleNav from './components/articleNav.vue'
 </script>
 
 <template>
   <nav class="home">
     <div class="user">首页</div>
-      <div class="nav">
-        <router-link class="transition-transform" to="/articleList/css" style="--i: -1">css</router-link>
-        <router-link class="transition-transform" to="/articleList/js" style="--i: 0">js</router-link>
-        <router-link class="transition-transform" to="/articleList/learn" style="--i: 1">learn</router-link>
-      </div>
+    <ArticleNav />
   </nav>
 </template>
 
@@ -23,52 +19,6 @@
   .user {
     width: 100%;
     height: 200px;
-  }
-
-  .nav {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    position: relative;
-    width: 50%;
-    height: 240px;
-    margin: 35px auto 10px;
-
-    &:active {
-      a:not(:active) {
-        background-color: #333;
-        border-color: #333;
-      }
-    }
-
-    &:hover {
-      a {
-        transform: rotate(calc(var(--i) * 10deg)) translate(calc(var(--i) * 120px), -30px);
-        box-shadow: 0 0 15px rgba(0, 0, 0, .3);
-        color: var(--home-word-hover);
-      }
-    }
-
-    a {
-      position: absolute;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      width: 180px;
-      height: 100%;
-      border-radius: 8px;
-      border: 10px solid var(--catalogue-bg);
-      background-color: var(--catalogue-bg);
-      transform-origin: 50% 100%;
-      filter: hue-rotate(calc(var(--i) * 50deg));
-      box-shadow: 0 0 15px rgba(0, 0, 0, .1);
-      color: var(--home-word-normal);
-
-      &:active {
-        translate: calc(var(--i) * 20px) -50px;
-        z-index: 9;
-      }
-    }
   }
 }
 </style>
